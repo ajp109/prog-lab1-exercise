a16dfab3f3bbc916bd9670b77ee5cf8ca14164c5
ajp109 <andrew.pomfret@york.ac.uk>
2022-08-02 13:54:00 +0000

    Correct code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include "mbed.h"
 
-int main() {
+int main()
+{
+    // Initialise the digital pins D2 and D3 as outputs
+    DigitalOut blue(D2);
+    DigitalOut red(D3);
+
+    // Initialise the digital pin BUTTON1 (the blue button) as an input
+    DigitalIn button(BUTTON1);
+    
+    // Loop forever...
     while (true) {
-        sleep();
+        
+        // Flash the blue LED: on for 100ms, off for 300ms
+        // Your code modifications should be made here
+        blue = true;
+        thread_sleep_for(100);
+        blue = false;
+        thread_sleep_for(300);
+        
     }
 }
